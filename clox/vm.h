f0e5e65d00bfe0eba3f0586d7936676cb64a423d
James Sutterfield <jsutterfield@uber.com>
2022-12-31 11:17:14 +0100

    Chapter 20 Hash Tables

--- clox/vm.h
+++ clox/vm.h
@@ -6,6 +6,8 @@
 #define CLOX_VM_H
 
 #include "chunk.h"
+#include "table.h"
+
 #define STACK_MAX 256
 
 
@@ -14,6 +16,7 @@ typedef struct {
     uint8_t *ip;
     Value stack[STACK_MAX];
     Value* stackTop;
+    Table strings;
     Obj* objects;
 } VM;
 
